cbaaa2c5e20efa9aac826b549304295985b3823d
Francisco Cruz <megafran@msn.com>
2022-03-30 08:12:38 -0500

    Build(env variables): improved setup of env variables for dev and prod environments

--- next.config.js
+++ next.config.js
@@ -1,8 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/block-explorer',
-  assetPrefix: '/block-explorer'
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+require('dotenv').config()
 
-module.exports = nextConfig
+const nextConfig = {
+  
+  reactStrictMode: true,
+      
+  future: {
+    webpack5: true,
+  },
+
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+
+    return config;
+  },
+
+  env: {
+    INFURA_ID: process.env.NEXT_PUBLIC_INFURA_ID,
+    ALGORAND_API: process.env.NEXT_PUBLIC_ALGORAND_API,
+    ALGORAND_ENDPOINT: process.env.NEXT_PUBLIC_ALGORAND_API_ENDPOINT
+  }}
+
+module.exports = (phase, { defaultConfig }) => {
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) { return nextConfig }
+
+  return { ...nextConfig, basePath: '/block-explorer', assetPrefix: '/block-explorer' }
+}
